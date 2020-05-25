fe2a4a9744f6196a6fe7bf1b7ea4d89981406cb9
AngieC <alovesj@hotmail.com>
2020-05-25 18:07:43 -0500

    Added header and nav elements styles, full width line chart added, section titles.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,29 @@
 var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31',],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Traffic',
+            data: [750, 1250, 900, 1780, 2500, 1740, 1900, 1200, 1800, 2350, 2000, 2350],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(116, 119, 191, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(116, 119, 191, 0.3)'
             ],
-            borderWidth: 1
+            borderWidth: 3
         }]
     },
+
     options: {
+      pointStyle: 'circle',
+      lineTension: 0,
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: false
+
                 }
             }]
         }
