1488ffd8d375b941bfd828dff6e45863ae0c14e5
AngieC <alovesj@hotmail.com>
2020-06-08 23:46:57 -0500

    Added chart variables in app.js, added social div, member div elements and styling.

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@
 
 
 
-var ctx = document.getElementById('TrafficChart');
-var TrafficChart = new Chart(ctx, {
-    type: 'line',
-    data: {
+var trafficCanvas = document.getElementById('TrafficChart');
+
+
+    let trafficData = {
         labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31',],
         datasets: [{
-            label: 'Traffic',
-            data: [150, 750, 900, 1780, 2000, 1740, 1900, 1200, 1800, 2350, 2000, 2350],
+            label: '',
+            data: [ 750, 900, 1780, 2000, 1740, 1900, 1200, 1800, 2350, 2000, 2350],
             backgroundColor: [
                 'rgba(116, 119, 191, 0.2)'
             ],
@@ -26,16 +26,19 @@ var TrafficChart = new Chart(ctx, {
             pointRadius: 6,
             borderWidth: 2
         }]
-    },
+    };
 
-    options: {
+    let trafficOptions = {
+      layout: {
+        padding: 30
+      },
       pointStyle: 'circle',
 
         scales: {
           xAxes: [{
           gridLines: {
               offsetGridLines: true
-          }
+          },
       }],
           yAxes: [{
             gridLines: {
@@ -43,17 +46,26 @@ var TrafficChart = new Chart(ctx, {
               },
                 ticks: {
                   beginAtZero: false
-
-
                 }
             }]
+        },
+        legend : {
+          display: false
         }
-    }
-});
-var ctx = document.getElementById('DailyChart');
-var DailyChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
+
+};
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+  });
+
+
+
+const dailyCanvas = document.getElementById('DailyChart');
+
+
+const dailyData = {
         labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
         datasets: [{
             data: [75, 125, 175, 150, 190, 220, 188],
@@ -62,12 +74,10 @@ var DailyChart = new Chart(ctx, {
             borderColor:'rgba(116, 119, 191, 1)',
             borderWidth: 2,
             barPercentage: .5
-
-
         }]
-    },
+    };
 
-    options: {
+  const dailyOptions = {
        layout: {
          padding: 100
        } ,
@@ -86,14 +96,24 @@ var DailyChart = new Chart(ctx, {
 
                 }
             }]
+        },
+        legend : {
+          display: false
         }
-    }
-});
+};
 
-var ctx = document.getElementById('UsersChart');
-var UsersChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
+var dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+  });
+
+
+var mobileCanvas = document.getElementById('MobileChart');
+
+
+
+    const mobileData = {
         labels: ['Phones', 'Tablets', 'Desktops'],
         datasets: [{
             data: [188,75, 275],
@@ -104,19 +124,33 @@ var UsersChart = new Chart(ctx, {
 
             ],
             borderColor:'rgba(116, 119, 191, 1)',
-            borderWidth: 2,
+            borderWidth: 0,
             hoverBackgroundColor: 'rgba(0, 85, 128, 1)'
         }]
-    },
+    };
 
-    options: {
+    const mobileOptions = {
       legend: {
-          position:'right'
+          position:'right',
+          labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+          }
       },
       layout: {
         padding: 100
       } ,
       cutoutPercentage: 60
 
-        }
-});
+    };
+    let mobileChart = new Chart(mobileCanvas, {
+        type: 'doughnut',
+        data: mobileData,
+        options: mobileOptions
+      });
+
+      const user = document.getElementById("userField");
+      const message = document.getElementById("messageField");
+      const send = document.getElementById("send");
+
+      // .addEventListener() {}
